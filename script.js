38644e91d19d4682d050ae49c0c7bb0f1c6800b9
mabel-chang <mabelchang00@gmail.com>
2023-05-01 00:02:44 -0400

    functionality of home

--- script.js
+++ script.js
@@ -15,100 +15,41 @@ As you move along the map, the long and lat changes <-- idk if i can T__T
 Need a way to store the long and lat that pops up (IDK if i can T__T)
   if not, not a big deal and scrap the functionality of the map
 
-Submit Button
-Response if you submit without putting any information in (standard input needed)
-  can you use the api to find this information?
-    To do this you would need to run the API first thing as they open the page
-    This will get the long and lat of the current location and move the map there
-  Can just have a standard long/lat that you will input
-
-
 }
 
-Click and have all relvent info now to store the data and load onto a new page
+Click and have all relvent info now to store the data and load onto a new page*/
 
-Menu Button
-*/
+function checkLatInput(in_lat){
+  if(in_lat == "" || in_lat == null){
+    return 2; //no input
 
-/*function injectHTML(list) {
-  console.log("fired injectHTML");
-  const target = document.querySelector("#restaurant_list");
-
-  target.innerHTML = "";
-  list.forEach((item) => {
-    const str = `<li>${item.name}</li>`;
-    target.innerHTML += str;
-  });
-}*/
-
-/*function filterList(list, query) {
-  return list.filter((item) => {
-    const lowerCaseName = item.name.toLowerCase();
-    const lowerCaseQuery = query.toLowerCase();
-
-    return lowerCaseName.includes(lowerCaseQuery);
-  });
-}*/
-
-/*function cutRestaurantList(list) {
-  console.log("fired cut list");
-  const range = [...Array(15).keys()];
-
-  return (newArray = range.map((item) => {
-    const index = getRandomIntInclusive(0, list.length - 1);
-    return list[index];
-  }));
-}*/
-
-/*function markerPlace(array, map){
-  console.log('array for markers', array);
-  
-  map.eachLayer((layer) => {
-    if (layer instanceof L.Marker) {
-      layer.remove();
-    }
-  });
-
-  array.forEach((item) => {
-    console.log('markerPlace', item);
-    if (item.geocoded_column_1){
-      const {coordinates} = item.geocoded_column_1;
-      L.marker([coordinates[1], coordinates[0]]).addTo(map);
-    }
-  })
-}
-
-function setViewWindow(array, map){
-  console.log('Lat and long: ', array[0]);
-  let carto = map;
-
-  if (array[0].geocoded_column_1){
-    const {coordinates} = array[0].geocoded_column_1;
-    carto = map.setView([coordinates[1], coordinates[0]], 13); //long, lat
   } else {
-    carto = map.setView([38.98, -76.93], 13); // set view to college park
-  }
-  
-  return carto;
-}
-*/
+    const int_in_lat = parseInt(in_lat);
 
-function check_response(in_long, in_lat){
-  let c = 0;
-  if(in_long >= -180 && in_long <= 180){
-    c = c+1;
+    if(int_in_lat != int_in_lat){ //return true if Nan
+      return 0; // failure because the input was string
+    } else if(int_in_lat >= -90 && int_in_lat <= 90){
+      return 1; // correct input
+    } else {
+      return 0;
+    }
   }
-  if(in_lat >= -90 && in_long <= 90){
-    c = c+2;
-  }
-  if(c==3){
-    return 3;
-  } else if (c==1){ //correct long
-    return 1;
-  } else if (c==2){ //correct lat
-    return 2;
+}
+
+function checkLongInput(in_long){
+  if(in_long == "" || in_long == null){
+    return 2; //no input
+
   } else {
-    return 0;
+    const int_in_long = parseInt(in_long);
+
+    if(int_in_long != int_in_long){ //return true if Nan
+      return 0; // failure because the input was string
+    } else if(int_in_long >= -180 && int_in_long <= 180){
+      return 1; // correct input
+    } else {
+      return 0;
+    }
   }
 }
 
@@ -123,6 +64,45 @@ function initMap(){
   return carto;
 }
 
+//Placeing marker onto the map
+function markerPlace(in_long, in_lat, map){
+  console.log('long and lat for marker', in_long + ", " + in_lat);
+  const check_lat = checkLatInput(in_lat);
+  const check_long = checkLongInput(in_long);
+
+  if (check_lat == 2){
+    in_lat = 0;
+  }
+  if (check_long == 2){
+    in_long = 0;
+  }
+
+  map.eachLayer((layer) => {
+    if(layer instanceof L.Marker){
+      layer.remove;
+    }
+  });
+
+  L.marker([in_lat, in_long]).addTo(map);
+}
+
+function setViewWindow(in_long, in_lat, map){
+  console.log('long and lat for marker', in_long + ", " + in_lat);
+  const check_lat = checkLatInput(in_lat);
+  const check_long = checkLongInput(in_long);
+ 
+  if (check_lat == 2){
+    in_lat = 0;
+  }
+  if (check_long == 2){
+    in_long = 0;
+  }
+
+  carto = map.setView([in_lat, in_long], 13);
+
+  return carto;
+}
+
 async function mainEvent() {
   console.log('localStorage check', localStorage.getItem("storedData"));
   const aboutButton = document.querySelector("#aboutButton");
@@ -132,6 +112,8 @@ async function mainEvent() {
   const errorMessage = document.querySelector("#error_message");
   let in_lat;
   let in_long;
+  let check_lat;
+  let check_long;
 
   /*Hide the error message*/
   errorMessage.style.display = "none";
@@ -146,80 +128,53 @@ async function mainEvent() {
 
   /*Take input from text input (long and lat)*/
   inLongitude.addEventListener("input", (event) => {
+     /*Hide the error message*/
+    errorMessage.style.display = "none";
     console.log("LONG INPUT: ", event.target.value);
     in_long = event.target.value;
-
+    check_long = checkLongInput(in_long);
+    check_lat = checkLatInput(in_lat);
+    if (check_long > 0 && check_lat > 0){
+      console.log(in_long);
+      markerPlace(in_long, in_lat, carto);
+      setViewWindow(in_long, in_lat, carto);
+    } else {
+      errorMessage.style.display = "inline-block";
+    }
   });
   inLatitidue.addEventListener("input", (event) => {
+     /*Hide the error message*/
+    errorMessage.style.display = "none";
     console.log("LAT INPUT: ", event.target.value);
     in_lat = event.target.value;
-
-    const checker = check_response(in_long, in_lat);
-    if (checker >= 2){
-
+    check_lat = checkLatInput(in_lat);
+    check_long = checkLongInput(in_long);
+    if (check_lat > 0 && check_long > 0){
+      console.log(in_lat);
+      markerPlace(in_long, in_lat, carto);
+      setViewWindow(in_long, in_lat, carto);
+    } else {
+      errorMessage.style.display = "inline-block";
     }
   });
 
   submitButton.addEventListener("click", (event) => {
-    /*console.log("Submit button pushed");
-    const checker = check_response(in_long, in_lat);
-    if(checker == 3){
+    console.log("Submit button pushed");
+    if (check_long > 0 && check_lat > 0){
+      if (check_lat == 2){
+        in_lat = 0;
+      }
+      if (check_long == 2){
+        in_long = 0;
+      }
+      console.log("long lat are valid", in_long + ", " + in_lat)
       localStorage.setItem('in_long', in_long);
       localStorage.setItem('in_lat', in_lat);
-
     } else {
-      errorMessage.classList.remove="hidden";
-    }*/
+      errorMessage.style.display = "inline-block";
+    }
     location.href="./pages/tracker";
   })
-
- /* submitButton.addEventListener("click", async (submitEvent) => {
-    console.log("Loading Data");
-    
-    location.href="./pages/tracker";
-    const results = await fetch(
-      "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
-    );
-    const storedList = await results.json();
-
-    localStorage.setItem("storedData", JSON.stringify(storedList));
-    parsedData = storedList;
-
-    if(storedList?.length > 0){
-      generateListButton.classList.remove("hidden");
-    }
-
-    // consoe.tabe(storedist);
-  });*/
-
-  /*const storedData = localStorage.getItem("storedData");
-
-  let parsedData = JSON.parse(storedData);
-
-  if (parsedData?.length > 0) {
-    generateListButton.classList.remove("hidden");
-  }
-  
-
-
-  submitButton.addEventListener("click", async (submitEvent) => {
-    console.log("Loading Data");
-    window.location.href="./pages/tracker";
-    const results = await fetch(
-      "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
-    );
-    const storedList = await results.json();
-
-    localStorage.setItem("storedData", JSON.stringify(storedList));
-    parsedData = storedList;
-
-    if(storedList?.length > 0){
-      generateListButton.classList.remove("hidden");
-    }
-
-    loadAnimation.style.display = "none";
-    // consoe.tabe(storedist);
-  });*/
 }
 
 document.addEventListener("DOMContentLoaded", async () => mainEvent());
