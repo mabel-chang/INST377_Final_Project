18b8c72b13516556b97c4bd704304bb16f199459
mabel-chang <mabelchang00@gmail.com>
2023-05-09 22:53:09 -0400

    organization

--- pages/tracker/script.js
+++ pages/tracker/script.js
@@ -24,6 +24,26 @@ in correct order of array
 3 <-- noon
 5, 7, 9, 3 <-- start of night
 
+  /*const sunrise = info_list[0];
+  const sunset = info_list[1];
+  const solarNoon = info_list[2];
+  const cTB = info_list[4]; // civil twilight begin and end
+  const cTE = info_list[5];
+  const nTB = info_list[6]; // nautrical twilight begin and end
+  const nTE = info_list[7];
+  const aTB = info_list[8]; // astronomical twilight begin and end // dawn
+  const aTE = info_list[9]; //dusk/*
+
+NEW ORDER!!!
+c=0
+0, 1, 2, 3, 4, 5, 6, 7, 8
+
+if(c==8){
+
+}
+
+CREATE FUNCTION TO REORDER THIS SHIT T__T
+
 */
 /*
 center globe to show the two points (unchangeing)
@@ -51,55 +71,52 @@ function antipode (in_lat, in_long){
 }
 
 async function mainEvent() {
+  /*DEFINE BUTTONS*/
   const homeButton = document.querySelector('#homeButton');
   const infoButton = document.querySelector('#aboutButton');
   const nextButton = document.querySelector('#nextButton');
+  const prevButton = document.querySelector('#prevButton');
   
+  /*GET INFO FROM API*/
+  const in_long = localStorage.getItem("in_long"); //get info from submission page
+  const in_lat = localStorage.getItem("in_lat"); //get info from submission page
   let info_list = [];
- 
-  const in_long = localStorage.getItem("in_long");
-  const in_lat = localStorage.getItem("in_lat");
- 
 
   console.log("long = ", in_long, " lat = ", in_lat);
-  document.getElementById('inLong').innerHTML = in_long; //changing text
-  document.getElementById('inLat').innerHTML = in_lat;//changing text
+  document.getElementById('inLong').innerHTML = in_long; //changing text in corrdinates
+  document.getElementById('inLat').innerHTML = in_lat;//changing text in corrdinates
 
-  const link = 'https://api.sunrise-sunset.org/json?lat='+ in_lat + '&lng=' + in_long;
-
+  const link = 'https://api.sunrise-sunset.org/json?lat='+ in_lat + '&lng=' + in_long; //get link
   const results = await fetch(
     link
   );
-  const currentList = await results.json();
+
+  const currentList = await results.json(); //Save the results
   for (var key in currentList.results) {
     info_list.push(JSON.stringify(currentList.results[key]));
   }
   console.log(info_list);
-  const sunrise = info_list[0];
-  const sunset = info_list[1];
-  const solarNoon = info_list[2];
-  const dayLen = info_list[3]; // day length
-  const cTB = info_list[4]; // civil twilight begin and end
-  const cTE = info_list[5];
-  const nTB = info_list[6]; // nautrical twilight begin and end
-  const nTE = info_list[7];
-  const aTB = info_list[8]; // astronomical twilight begin and end // dawn
-  const aTE = info_list[9]; //dusk
 
+  /*GET INFO ON OPPOSITE LOCATION*/
   const{op_lat, op_long} = antipode(in_lat, in_long);
-
   console.log("op_long= ", op_long, "op_lat = ", op_lat);
-  document.getElementById('opLong').innerHTML = op_long; //changing text
-  document.getElementById('opLat').innerHTML = op_lat;//changing text
 
-  homeButton.addEventListener("click", (event) => {
+  document.getElementById('opLong').innerHTML = op_long; //changing text in corrdinates
+  document.getElementById('opLat').innerHTML = op_lat;//changing text in corrdinates
+
+  /*BUTTON FUNCTIONALITY*/
+  //menu buttons
+  homeButton.addEventListener("click", (event) => {//home button
     location.href="../../";
   })
-  infoButton.addEventListener("click", (event) => {
+  infoButton.addEventListener("click", (event) => {//about button
     location.href="./../about";
   })
-
+  //going through the list
   nextButton.addEventListener("click", async (submitEvent) => {
+    document.getElementById('tempOutput').innerHTML = "Sunset Time is: " + sunset; //changing text
+  });
+  prevButton.addEventListener("click", async (submitEvent) => {
     document.getElementById('tempOutput').innerHTML = "Sunset Time is: " + sunset; //changing text
   });
 }
